1a90be5e322e28c0fae8ec1dc4b1d546db070c8e
Kacper Mazur <mazur.kacper9680@gmail.com>
2022-07-21 22:35:27 +0200

    Added functions to combine templates with context dicts for order and orders

--- pizzeria/pizzas/views.py
+++ pizzeria/pizzas/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .models import Pizza
 
 def index(request):
-    """Home page for pizzas"""
+    """Home page for the Pizzeria"""
     return render(request, 'pizzas/index.html')
+
+def orders(request):
+    """Page for orders"""
+    orders = Pizza.objects.order_by('date_added')
+    context = {'orders': orders}
+    return render(request, 'pizzas/orders.html', context )
+
+def order(request, order_id):
+    """Showing the details of each order"""
+    order = Pizza.objects.get(id=order_id)
+    toppings = order.topping_set.all()
+    context = {'order': order, 'toppings': toppings}
+    return render(request, 'pizzas/order.html', context)
