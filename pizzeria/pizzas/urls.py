69909570dcdf9a1a26ac54dec44968b794d92a36
Kacper Mazur <mazur.kacper9680@gmail.com>
2022-07-21 22:33:24 +0200

    Added urls for all orders and each order

--- pizzeria/pizzas/urls.py
+++ pizzeria/pizzas/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from . import views
+from . import views 
 
 ## Helping django distinguish this urls.py from other with the same name
 app_name = 'pizzas'  
@@ -7,5 +7,11 @@ app_name = 'pizzas'
 # List of pages
 urlpatterns = [
     # Home page
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+
+    # Showing all the orders
+    path('orders/', views.orders, name='orders'),
+
+    # Details for each order
+    path('orders/<int:order_id>/', views.order, name='order')
 ]
