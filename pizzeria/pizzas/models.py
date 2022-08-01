84d7a597c041582491f10d4e10ec426ee840b813
Kacper Mazur <mazur.kacper9680@gmail.com>
2022-08-01 21:17:04 +0200

    Added the employee of an order

--- pizzeria/pizzas/models.py
+++ pizzeria/pizzas/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Pizza (models.Model):
     order = models.CharField(max_length=50)
     date_added = models.DateTimeField(auto_now_add=True)
+    order_taker = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.order
